9f85afda3125255b9ead566846920916a5a0b46a
Jackson Ming Hu <huming2207@gmail.com>
2024-05-14 22:54:29 +1000

    net_mon: implement skeleton

--- esp_transport_net_mon.c
+++ esp_transport_net_mon.c
@@ -1,1 +1,42 @@
+#include <esp_transport.h>
 #include "esp_transport_net_mon.h"
+
+static int net_mon_connect(esp_transport_handle_t handle, const char *host, int port, int timeout_ms)
+{
+    return 0;
+}
+
+static int net_mon_write(esp_transport_handle_t handle, const char *buffer, int len, int timeout_ms)
+{
+    return 0;
+}
+
+static int net_mon_read(esp_transport_handle_t handle, char *buffer, int len, int timeout_ms)
+{
+    return 0;
+}
+
+static int net_mon_poll_read(esp_transport_handle_t handle, int timeout_ms)
+{
+    return 0;
+}
+
+static int net_mon_poll_write(esp_transport_handle_t handle, int timeout_ms)
+{
+    return 0;
+}
+
+static int net_mon_close(esp_transport_handle_t handle)
+{
+    return 0;
+}
+
+static int net_mon_destroy(esp_transport_handle_t handle)
+{
+    return 0;
+}
+
+static int net_mon_connect_async(esp_transport_handle_t handle, const char *host, int port, int timeout_ms)
+{
+    return 0;
+}
--- esp_transport_net_mon.h
+++ esp_transport_net_mon.h
@@ -1,2 +1,19 @@
 #pragma once
 
+#include <stdint.h>
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint32_t timeout_threshold_ms;
+    uint32_t slowdown_threshold_ms;
+} transport_net_mon_config_t;
+
+esp_err_t esp_transport_net_monitor_create();
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
