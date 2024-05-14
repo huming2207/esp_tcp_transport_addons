b27e13b7111e39304675a429c7b996e8dd1df436
Jackson Ming Hu <huming2207@gmail.com>
2024-05-15 09:34:47 +1000

    netmon: implement config

--- esp_transport_net_mon.c
+++ esp_transport_net_mon.c
@@ -1,6 +1,10 @@
 #include <esp_transport.h>
 #include "esp_transport_net_mon.h"
 
+typedef struct {
+
+} transport_net_mon_instance_t;
+
 static int net_mon_connect(esp_transport_handle_t handle, const char *host, int port, int timeout_ms)
 {
     return 0;
@@ -39,4 +43,9 @@ static int net_mon_destroy(esp_transport_handle_t handle)
 static int net_mon_connect_async(esp_transport_handle_t handle, const char *host, int port, int timeout_ms)
 {
     return 0;
+}
+
+esp_err_t esp_transport_net_monitor_create(const transport_net_mon_config_t *config, esp_transport_handle_t *new_netmon_handle, esp_transport_handle_t parent_handle, esp_transport_handle_t child_handle)
+{
+    return ESP_OK;
 }
--- esp_transport_net_mon.h
+++ esp_transport_net_mon.h
@@ -8,11 +8,17 @@ extern "C" {
 #endif
 
 typedef struct {
-    uint32_t timeout_threshold_ms;
-    uint32_t slowdown_threshold_ms;
+    esp_transport_handle_t parent_handle;
+    esp_transport_handle_t child_handle;
+    uint32_t timeout_ms_thresh;
+    uint32_t slowdown_ms_thresh;
+    uint32_t timeout_count_thresh;
+    uint32_t slowdown_count_thresh;
+    uint32_t open_retry_count_thresh;
+    uint64_t instance_id;
 } transport_net_mon_config_t;
 
-esp_err_t esp_transport_net_monitor_create();
+esp_err_t esp_transport_net_monitor_create(const transport_net_mon_config_t *config, esp_transport_handle_t *new_netmon_handle, esp_transport_handle_t parent_handle, esp_transport_handle_t child_handle);
 
 #ifdef __cplusplus
 }
